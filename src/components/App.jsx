ba77d86cbf127e75cd7fdabb807ecbcf2e73c933
Serhii Rybak <rybaksw@gmail.com>
2022-07-24 16:19:25 +0300

    add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import { GlobalStyle } from 'styles/GlobalStyle';
+
 export const App = () => {
-  return;
+  return (
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<h2>Home</h2>} />
+        <Route path="/movies" element={<h2>Movies</h2>} />
+        <Route path="/movies/:movieId" element={<h2>MovieDetails</h2>}>
+          <Route path="cast" element={<h3>Cast</h3>} />
+          <Route path="reviews" element={<h3>Reviews</h3>} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
